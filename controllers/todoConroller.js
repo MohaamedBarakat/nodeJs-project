76f09edeb27171105248b6afa1c38a6c9b822398
MohaamedBarakat <muhammad.barakat70@gmail.com>
2020-10-25 21:11:01 +0200

    adding ajax file , adding som changes in app.js ,adding delete and Post requests

--- controllers/todoConroller.js
+++ controllers/todoConroller.js
@@ -1,11 +1,28 @@
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'write some code for me' }];
+
 module.exports = function(app) {
     app.get('/todo', function(req, res) {
-        res.render('todo');
+        res.render('todo', { todos: data });
     });
-    app.post('/todo', function(req, res) {
-
+    app.post('/todo', urlencodedParser, function(req, res) {
+        data.push(req.body);
+        //console.log(data);
+        res.json({ todos: data });
+        console.log("data after adding item ");
+        console.log(data);
     });
-    app.delete('/todo', function(req, res) {
+    app.delete('/todo/:item', function(req, res) {
 
+        data = data.filter(function(todo) {
+
+            return todo.item !== req.params.item.trim();
+        });
+        res.json({ todos: data });
+        console.log("data after removing item ");
+        console.log(data);
     });
 }
