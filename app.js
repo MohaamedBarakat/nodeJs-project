c7ce7426f7943d237621fd657024cced6196535c
MohaamedBarakat <muhammad.barakat70@gmail.com>
2020-10-25 17:37:19 +0200

    adding cs file and some edit in app and add logo.png phot o

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var todoController = require('./controllers/todoConroller');
 var app = express();
 
 // Set template engine
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // Setup static files
-app.use('/assets', express.static('./public'));
+app.use('/assets', express.static('./public/assets'));
 //  fire controller
 todoController(app);
 
 //Listen to the Port
-app.listen(3000, "127.0.0.1");
-console.log(' I am listening to the port 3000');
+app.listen(3001, "127.0.0.1");
+console.log(' I am listening to the port 3001');
 
 var bodyParser = require('body-parser');
